cb4dad48ca301c7d1d67d869005e63424a95c5e1
Samadsust71 <samadsust71@gmail.com>
2024-12-08 10:58:11 +0600

    add cors origin

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-  origin: "https://cine-verse-b249e.web.app"
-}));
+app.use(cors(
+  {
+    origin: "https://cine-verse-b249e.web.app"
+   }
+));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qo68l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
